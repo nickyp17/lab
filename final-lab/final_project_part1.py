dbacad7ac4f26cbea6e6163b95dc0cec5add3131
Nicholas Poulidis <71410380+nickyp17@users.noreply.github.com>
2023-03-30 19:50:39 -0400

    feat: implementing dijsktra_approx which relaxes a node a max of k times

--- final-lab/final_project_part1.py
+++ final-lab/final_project_part1.py
@@ -57,6 +57,35 @@ def dijkstra(G, source):
     return dist
 
 
+def dijkstra_approx(G, source, k):
+    pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
+    dist = {} #Distance dictionary
+    relax = {} #Relaxation dictionary
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    #Initialize priority queue/heap and distances
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+        relax[node] = 0
+    Q.decrease_key(source, 0)
+
+    #Meat of the algorithm
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+        for neighbour in G.adj[current_node]:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour] and relax[current_node] < k:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+                relax[neighbour] = relax[current_node] + 1
+    return dist
+
+
+
 def bellman_ford(G, source):
     pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
     dist = {} #Distance dictionary
